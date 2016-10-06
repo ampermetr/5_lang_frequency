ac2124fd2f93211730135872e434cc80f7e37457
ampermetr <mlechnik1988@mail.ru>
2016-10-06 12:56:10 +0000

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,5 +1,5 @@
 import os
 
 if __name__ == '__main__':
-    filepath = input('Введи путь к файлу')
-    os.system ("cat " + filepath + " | tr ' ' '\n' | sort | uniq -c | sort -rn | head -10")
+    filepath = input('Введи путь к файлу')    
+    os.system ("sed 's/[ ,./!@#$%^&*()№:;_-–]/\\n/g' " + filepath + " | tr -s '\n' | tr '[A-Z]' '[a-z]' | sort | uniq -c | sort -rn | head -10")
